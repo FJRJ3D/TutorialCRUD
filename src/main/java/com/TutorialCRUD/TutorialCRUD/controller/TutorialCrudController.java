9e79fb79d76e06030d0f6f0e8aeb82df048e3de5
Angel Ponce Reguera <angelillopr98@gmail.com>
2024-07-09 14:13:00 +0200

    Added tutorials

--- src/main/java/com/TutorialCRUD/TutorialCRUD/controller/TutorialCrudController.java
+++ src/main/java/com/TutorialCRUD/TutorialCRUD/controller/TutorialCrudController.java
@@ -1,6 +1,7 @@
 package com.TutorialCRUD.TutorialCRUD.controller;
 
 
+import com.TutorialCRUD.TutorialCRUD.model.Tutorial;
 import com.TutorialCRUD.TutorialCRUD.service.TutorialCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
-public class TutorialCrudController {
+
+    public class TutorialCrudController {
     @Autowired
     TutorialCrudService tutorialCrudService;
 
+    @PostMapping (path = "/tutorials")
+    public Tutorial createTutorial(@RequestBody Tutorial newTutorial){
+        return tutorialCrudService.createTutorial(newTutorial);
+    }
 
 }
--- src/main/java/com/TutorialCRUD/TutorialCRUD/service/TutorialCrudService.java
+++ src/main/java/com/TutorialCRUD/TutorialCRUD/service/TutorialCrudService.java
@@ -1,5 +1,6 @@
 package com.TutorialCRUD.TutorialCRUD.service;
 
+import com.TutorialCRUD.TutorialCRUD.model.Tutorial;
 import com.TutorialCRUD.TutorialCRUD.repository.ITutorialCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TutorialCrudService {
+
     @Autowired
     ITutorialCrudRepository iTutorialCrudRepository;
 
-
+    public Tutorial createTutorial(Tutorial newTutorial){
+        return iTutorialCrudRepository.save(newTutorial);
+    }
 }
